033d1d32e9ac11a8603956bcc94f819b1a1e1fe9
Quabynah Bilson Jr <84394923+quabynah-bilson@users.noreply.github.com>
2023-09-08 02:46:42 +0000

    Update redis.go

--- adapters/token/datastore/redis.go
+++ adapters/token/datastore/redis.go
@@ -87,7 +87,7 @@ func (db *RedisTokenDatabase) ValidateToken(authToken, accountID string) error {
 // DeleteToken invalidates the given token.
 func (db *RedisTokenDatabase) DeleteToken(accountID string) error {
 	// set context with timeout of 5 seconds
-	ctx, cancel := context.WithTimeout(context.Background(), 105*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	// delete the token
